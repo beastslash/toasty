e7658706af51f8191039709d90f6858ca0221ff2
Christian <37517470+Draguwro@users.noreply.github.com>
2020-08-31 08:37:52 -0400

    Create eval.js
    Closes #3

---
+++ commands/eval.js
@@ -0,0 +1,59 @@
+const Command = require("../commands");
+
+function userAllowedToEval(bot, userId) {
+  
+  // Check if they have a role
+  const AdminGuildId = process.env.ADMIN_GUILD_ID;
+  const AdminRoleId = process.env.ADMIN_ROLE_ID;
+  
+  if (AdminGuildId && AdminRoleId) {
+    
+    function getGuild(guild) {
+      if (guild.id === AdminGuildId) {
+        return guild;
+      };
+    };
+    
+    function getMember(member) {
+      if (member.id === userId) {
+        return member;
+      };
+    };
+    
+    function getRole(roleId) {
+      if (roleId === AdminRoleId) {
+        return roleId;
+      };
+    };
+    
+    const Guild = bot.guilds.find(getGuild);
+    const Member = Guild ? Guild.members.find(getMember) : undefined;
+    const Role = Member ? Member.roles.find(getRole) : undefined;
+    
+    if (Role) {
+      return true;
+    };
+    
+  };
+  
+  return false;
+  
+};
+
+module.exports = function() {
+  new Command.new("eval", ["evaluate"], (bot, args, msg) => {
+    
+    // Make sure they're allowed to eval
+    if (!userAllowedToEval(bot, msg.author.id)) {
+      return;
+    };
+    
+    // Run the command
+    try {
+      eval(args);
+    } catch (err) {
+      msg.channel.createMessage("Something happened!!1 \n```" + err + "\n```");
+    };
+    
+  });
+};
